8f2902219b3d6f4ac3b7543229021d07b25e3f73
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2022-03-06 20:49:08 +0630

    #google login

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,83 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Exception;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+  
+class GoogleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+        
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleGoogleCallback()
+    {
+        // try {
+            info("come");
+            $googleUser = Socialite::driver('google')->stateless()->user();
+            info("come");
+
+
+            $user = User::where('google_id', $googleUser->id)->first();
+
+            if ($user) {
+
+                Auth::login($user);
+
+                return redirect()->intended('dashboard');
+
+            } else {
+                $user = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'google_id' => $googleUser->id,
+                ]);
+
+                Auth::login($user);
+
+                return redirect()->intended('dashboard');
+            }
+      
+            // $user = Socialite::driver('google')->user();
+       
+            // $finduser = User::where('google_id', $user->id)->first();
+       
+            // if($finduser){
+       
+            //     Auth::login($finduser);
+      
+            //     return redirect()->intended('dashboard');
+       
+            // }else{
+            //     $newUser = User::create([
+            //         'name' => $user->name,
+            //         'email' => $user->email,
+            //         'google_id'=> $user->id,
+            //     ]);
+      
+            //     Auth::login($newUser);
+      
+            //     return redirect()->intended('dashboard');
+            // }
+      
+        // } catch (Exception $e) {
+        //     info("herer");
+        //     dd($e->getMessage());
+        // }
+    }
+}
